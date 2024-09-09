4d057eccabc6677a43865592d4d721d760966a77
Florian Maurer <maurer@fh-aachen.de>
2024-09-09 14:39:15 +0200

    add performance evaluation script

---
+++ paho-sub.py
@@ -0,0 +1,68 @@
+# python 3.11
+
+import random
+import time
+
+import paho.mqtt.client as paho
+
+import logging
+
+logging.basicConfig(format="%(asctime)s;%(levelname)s;%(message)s")
+
+logger = logging.getLogger(__name__)
+
+broker = 'localhost'
+port = 1883
+topic = "python/mqtt"
+# Generate a Client ID with the publish prefix.
+client_id = f'publish-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc, pa):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    def on_message(client, userdata, message):
+        logger.warning("got message %s", message)
+
+    client = paho.Client(paho.CallbackAPIVersion.VERSION2, client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(broker, port)
+    client.subscribe(topic)
+    
+    return client
+
+
+def publish(client):
+    msg_count = 1
+    while True:
+        #time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+        if msg_count > 5:
+            break
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    time.sleep(10)
+    #publish(client)
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    run()
---
+++ phd.py
@@ -0,0 +1,103 @@
+import asyncio
+import logging
+import time
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from tests.integration_tests.test_single_container_termination import (
+    distribute_ping_pong_test,
+    distribute_ping_pong_test_timestamp,
+)
+
+results = []
+
+rename = {
+    "distribute_ping_pong_test_timestamp": "clock synchronisation",
+    "distribute_ping_pong_test": "direct ping pong"
+}
+
+EXEC_RUNTIME_STUDY = True
+
+if EXEC_RUNTIME_STUDY:
+    for func in [distribute_ping_pong_test, distribute_ping_pong_test_timestamp]:
+        for max_count in [1, 2, 25, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 5000, 10000]:
+            for connection_type in ["tcp", "mqtt"]:
+                if max_count > 1000 and connection_type == "mqtt":
+                    continue
+                print(connection_type, max_count, func.__name__)
+                t = time.time()
+                asyncio.run(func(connection_type, max_count=max_count))
+                duration = time.time() - t
+                results.append([rename[func.__name__], connection_type, max_count, duration])
+else:
+    logging.getLogger("mango").setLevel(logging.INFO)
+    logging.basicConfig(format="%(asctime)s;%(levelname)s;%(message)s")
+    connection_type = "mqtt"
+    max_count=10
+    func = distribute_ping_pong_test_timestamp
+    print(connection_type, max_count, func.__name__)
+    t = time.time()
+    asyncio.run(func(connection_type, max_count=max_count))
+    duration = time.time() - t
+    results.append([func.__name__, connection_type, max_count, duration])
+
+df = pd.DataFrame(results, columns = ["function", "connection_type", "count", "duration"])
+df["roundtrip_per_second"] = df["count"]/df["duration"]
+print(df)
+if EXEC_RUNTIME_STUDY:
+    df.to_csv("runtime.csv", index=None)
+
+SAVE_FIG=True
+
+if SAVE_FIG:
+    df = pd.read_csv("runtime.csv")
+    pivot_df = df.pivot(columns=["function", "connection_type"], index="count")
+    # substract duration with 1 message (shows overhead)
+    startup_duration = pivot_df["duration"].loc[1]
+    print(startup_duration)
+    diff = pivot_df["duration"] - pivot_df["duration"].loc[1]
+    # recalculate roundtrip per second
+    inverse_df = 1/diff.div(diff.index.values, axis=0)
+    pivot_df["roundtrip_per_second"] = inverse_df
+    # remove index=1 which is infinity
+    pivot_df = pivot_df.drop(1)
+    pivot_df["roundtrip_per_second"][pivot_df.index <= 1000].plot(figsize=(10,5), grid=True)
+    plt.ylabel("roundtrips per second")
+    plt.title("roundtrips by technology and method")
+    plt.savefig("roundtrip_per_seconds.svg")
+
+    pivot_df["duration"].plot(figsize=(10,5), grid=True)
+    plt.savefig("duration.svg")
+
+
+# Function to create LaTeX table from Series with MultiIndex
+def series_to_latex(series):
+    latex_table = "\\begin{table}[h]\n\\centering\n\\begin{tabular}{lll}\n\\hline\n"
+    latex_table += "\\textbf{Function} & \\textbf{Connection Type} & \\textbf{Value} \\\\\n\\hline\n"
+
+    current_function = ""
+    for (function, connection_type), value in series.items():
+        if function != current_function:
+            if current_function:
+                latex_table += "\\hline\n"
+            latex_table += f"\\multirow{{2}}{{*}}{{{function}}} & {connection_type} & {value:.6f} \\\\\n"
+            current_function = function
+        else:
+            latex_table += f" & {connection_type} & {value:.6f} \\\\\n"
+
+    latex_table += "\\hline\n\\end{tabular}\n"
+    latex_table += "\\caption{Function and Connection Type Data}\n"
+    latex_table += "\\label{tab:startup_duration}\n"
+    latex_table += "\\end{table}"
+
+    return latex_table
+
+
+# Generate and print the LaTeX table
+latex_output = series_to_latex(startup_duration)
+print(latex_output)
+
+# Optionally, save to a file
+with open('startup_duration.tex', 'w') as f:
+    f.write(latex_output)
